63d8e4642e74809279c45113d1260f93464e69df
Marina <itsmarinams@gmail.com>
2023-05-22 19:12:07 +0200

    Class BranchTesting.java added: - Constructor w id - Getters & setters

--- src/BranchTesting.java
+++ src/BranchTesting.java
@@ -3,4 +3,31 @@ public class BranchTesting {
     private int id;
     private double  aleix;
 
+    public BranchTesting(int id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAleix() {
+        return aleix;
+    }
+
+    public void setAleix(double aleix) {
+        this.aleix = aleix;
+    }
 }
